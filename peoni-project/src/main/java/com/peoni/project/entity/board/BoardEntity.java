23e05a862a9ed1e4cb394a9bc367ada1f2ec6149
jihyeon <wlgusl147@naver.com>
2025-07-18 12:34:32 +0900

    통합 게시판을 위한 boardEntity추가

---
+++ peoni-project/src/main/java/com/peoni/project/entity/board/BoardEntity.java
@@ -0,0 +1,66 @@
+package com.peoni.project.entity.board;
+
+import com.peoni.project.entity.comm.BaseEntity;
+import com.peoni.project.entity.member.MemberEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@SequenceGenerator(
+		name = "BOARD_SEQ_GEN",
+		sequenceName = "board_seq",
+		initialValue = 1,
+		allocationSize = 1)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class BoardEntity extends BaseEntity{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+					generator = "BOARD_SEQ_GEN")
+	private Long boardId;
+	
+	@Column(name = "title", nullable = false, length = 100)
+	private String title;
+	
+	@Column(name = "content", nullable = false, length = 2000)
+	private String content;
+	
+	@Column(name = "board_type", nullable = false)
+	private String boardType;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "mno", nullable = false)
+	private MemberEntity writer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
