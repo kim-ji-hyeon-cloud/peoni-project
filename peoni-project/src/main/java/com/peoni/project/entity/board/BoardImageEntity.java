b6aa1f5d911e1a53ff11547f1c6cc9699ac448d7
jihyeon <wlgusl147@naver.com>
2025-07-18 12:53:51 +0900

    boardimage, boardreview 엔티티 추가

---
+++ peoni-project/src/main/java/com/peoni/project/entity/board/BoardImageEntity.java
@@ -0,0 +1,63 @@
+package com.peoni.project.entity.board;
+
+import com.peoni.project.entity.comm.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@SequenceGenerator(
+		name = "BOARD_IMAGE_SEQ_GEN",
+		sequenceName = "board_image-seq",
+		initialValue = 1,
+		allocationSize = 1)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "board")
+public class BoardImageEntity extends BaseEntity{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+					generator = "BOARD_IMAGE_SEQ_GEN")
+	private Long imageId;
+	
+	@Column(name = "uuid", nullable = false)
+	private String uuid;
+	
+	@Column(name = "image_name", nullable = false)
+	private String imageName;
+	
+	@Column(name = "path", nullable = false)
+	private String path;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "board_id", nullable = false)
+	private BoardEntity board;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ peoni-project/src/main/java/com/peoni/project/entity/board/BoardReviewEntity.java
@@ -0,0 +1,62 @@
+package com.peoni.project.entity.board;
+
+import com.peoni.project.entity.comm.BaseEntity;
+import com.peoni.project.entity.member.MemberEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@SequenceGenerator(
+		name = "BOARD_REVIEW_SEQ_GEN",
+		sequenceName = "board_review_seq",
+		initialValue = 1,
+		allocationSize = 1)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"board, writer"})
+public class BoardReviewEntity extends BaseEntity{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+					generator = "BOARD_REVIEW_SEQ_GEN")
+	private Long reviewId;
+	
+	@Column(name = "content", nullable = false, length = 1000)
+	private String content;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "board_id", nullable = false)
+	private BoardEntity board;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "mno", nullable = false)
+	private MemberEntity writer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
